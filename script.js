fa39c7a6aff41b335c8556cb200e42a397f8ecc7
Misael Garcia <110581963+Misael2004@users.noreply.github.com>
2025-07-04 11:05:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ paypal.Buttons({
     return actions.order.create({
       purchase_units: [{
         amount: {
-          value: '25.00' // Precio del producto
+          value: '1.00' // Precio del producto
         }
       }]
     });

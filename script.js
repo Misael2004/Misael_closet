c794eb31c464fc414b900d97d9862d4136dc342a
Misael Garcia <110581963+Misael2004@users.noreply.github.com>
2025-07-04 10:56:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+paypal.Buttons({
+  createOrder: function(data, actions) {
+    return actions.order.create({
+      purchase_units: [{
+        amount: {
+          value: '25.00' // Precio del producto
+        }
+      }]
+    });
+  },
+  onApprove: function(data, actions) {
+    return actions.order.capture().then(function(details) {
+      alert('Pago realizado por ' + details.payer.name.given_name);
+      console.log(details);
+    });
+  },
+  onError: function(err) {
+    console.error('Error en el pago', err);
+    alert('Hubo un error en el pago. Intenta de nuevo.');
+  }
+}).render('#paypal-button-container');
